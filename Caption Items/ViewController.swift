d31b9eb5f8362f7b45af69660e97cedcd790d53a
Mihail87 <leontemihai@gmail.com>
2019-09-23 15:18:38 +0300

    Project setup

---
+++ Caption Items/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Caption Items
+//
+//  Created by Mihai Leonte on 9/23/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
